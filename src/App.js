24e2c831bf937e2eb0b22d54c3d0eef3abd515f3
Code Artistry <reedcodes@protonmail.com>
2018-11-22 02:03:47 -0500

    Data fetching example with HN API w/ useState, useEffect (async / await, with componentDidUpdate functionality), useRef, form, loading state, error handling

--- src/App.js
+++ src/App.js
@@ -1,28 +1,79 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useRef } from "react";
+import axios from "axios";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+function App() {
+  const [data, setData] = useState({ hits: [] });
+  const [query, setQuery] = useState("react hooks");
+  const [url, setUrl] = useState(
+    "http://hn.algolia.com/api/v1/search?query=redux"
+  );
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState();
+  const searchEl = useRef(null);
+
+  useEffect(
+    () => {
+      getData();
+    },
+    [url]
+  );
+
+  const getData = async () => {
+    setLoading(true);
+
+    try {
+      const { data } = await axios(
+        `http://hn.algolia.com/api/v1/search?query=${query}`
+      );
+      setData(data);
+    } catch (err) {
+      setError(err);
+    }
+
+    setLoading(false);
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    setUrl(`http://hn.algolia.com/api/v1/search?query=${query}`);
+  };
+
+  const handleClearSearch = () => {
+    setQuery("");
+    // `current` points to the mounted text input element
+    searchEl.current.focus();
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={query}
+          ref={searchEl}
+          onChange={event => setQuery(event.target.value)}
+        />
+        <button type="submit">Search</button>
+        <button type="button" onClick={handleClearSearch}>
+          Clear
+        </button>
+      </form>
+
+      {loading ? (
+        <div>Loading...</div>
+      ) : (
+        <ul>
+          {data.hits.map(item => (
+            <li key={item.objectID}>
+              <a href={item.url}>{item.title}</a>
+            </li>
+          ))}
+        </ul>
+      )}
+
+      {error && <div>Something went wrong...</div>}
+    </>
+  );
 }
 
 export default App;
