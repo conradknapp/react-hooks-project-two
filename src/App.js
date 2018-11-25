bdb017f80960765847404f750b254094f1bd7dbb
Conrad Knapp <conrad.knapp@bc.edu>
2018-11-25 16:06:38 -0500

    Improves styling with Tailwind

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     "http://hn.algolia.com/api/v1/search?query=redux"
   );
   const [loading, setLoading] = useState(false);
-  const [error, setError] = useState();
+  const [error, setError] = useState("");
   const searchEl = useRef(null);
 
   useEffect(
@@ -45,34 +45,52 @@ function App() {
   };
 
   return (
-    <>
-      <form onSubmit={handleSubmit}>
+    <div className="container max-w-md mx-auto p-4 m-2 bg-purple-lightest shadow-lg rounded">
+      <img
+        src="https://icon.now.sh/react/c0c"
+        alt="React"
+        className="float-right h-12"
+      />
+      <h1 className="text-grey-darkest font-thin">Hooks News</h1>
+      <form onSubmit={handleSubmit} className="mb-2">
         <input
           type="text"
           value={query}
           ref={searchEl}
           onChange={event => setQuery(event.target.value)}
+          className="border p-1 rounded"
         />
-        <button type="submit">Search</button>
-        <button type="button" onClick={handleClearSearch}>
+        <button type="submit" className="m-1 p-1 bg-orange rounded">
+          Search
+        </button>
+        <button
+          type="button"
+          onClick={handleClearSearch}
+          className="p-1 bg-teal text-white rounded"
+        >
           Clear
         </button>
       </form>
 
       {loading ? (
-        <div>Loading...</div>
+        <div className="font-bold text-orange-dark">Loading...</div>
       ) : (
-        <ul>
+        <ul className="list-reset leading-normal">
           {data.hits.map(item => (
             <li key={item.objectID}>
-              <a href={item.url}>{item.title}</a>
+              <a
+                href={item.url}
+                className="text-indigo-dark hover:text-indigo-darkest"
+              >
+                {item.title}
+              </a>
             </li>
           ))}
         </ul>
       )}
 
-      {error && <div>Something went wrong...</div>}
-    </>
+      {error && <div className="text-red font-bold">{error.message}</div>}
+    </div>
   );
 }
 
